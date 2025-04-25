b791a3cee113c0cbc1687a1bf95c4cec4bfd57e3
Areg Abrahamyan <areg.abrahamyan3@gmail.com>
2025-04-25 11:30:57 -0800

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-"use client";
-import { CustomKanban } from "@/components/Kanban"
+// app/page.tsx
+
+import Kanban from "@/components/Kanban";
 
 export default function Home() {
   return (
-   <CustomKanban />
+    <main className="min-h-screen bg-neutral-950 p-6 text-white">
+      <h1 className="mb-8 text-3xl font-bold text-center">Kanban Board</h1>
+      <Kanban />
+    </main>
   );
 }
